fa7b3915f44f131112d7ee9d21a87cd99a38ec2b
Karen Yin Guðnadóttir <kareng24@ru.is>
2026-02-12 16:20:18 +0000

    ADD:: heap test

--- heapTest.cpp
+++ heapTest.cpp
@@ -7,19 +7,40 @@
 #include "heap.hpp"
 
 int main() {
-    MinHeap<int> h;
-    h.push(4);
-    h.getAll();
-    h.push(7);
-    h.push(1);
-    h.getAll();
-    std:: cout << "PEEK: " << h.peek() << std::endl;
-    h.push(2);
-    h.pop();
-    h.getAll();
-    
+    int q;
+    std::cin >> q;
+
+    MinHeap<int> h[1001];
+
+    for (int i = 0; i < q; i++) {
+        int id;
+        char op;
+        std::cin >> id >> op;
+
+        if (op == 'a') {
+            int copy_id;
+            std::cin >> copy_id;
+            h[id] = h[copy_id];
+        }
+        else if (op == '+') {
+            int value;
+            std::cin >> value;
+            h[id].push(value);
+        }
+        else if (op == '-') {
+            h[id].pop();
+        }
+        else if (op == 'p') {
+            std::cout << h[id].peek() << std::endl;
+        }
+        else if (op == 's') {
+            std::cout << h[id].size() << std::endl;
+        }
+    }
+
     return 0;
 }
+
 
 /*  RUN
 g++ -std=c++98 heapTest.cpp -o heap2
