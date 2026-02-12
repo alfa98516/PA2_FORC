c94bb14aceeba21b2f4c2046df0abc3622cf768b
Karen Yin Guðnadóttir <kareng24@ru.is>
2026-02-12 17:25:07 +0000

    UPDATE:: dequeTest

--- dequeTest.cpp
+++ dequeTest.cpp
@@ -7,32 +7,49 @@
 #include "dequeue.hpp"
 
 int main() {
+    int q;
+    std::cin >> q;
 
-    Dequeue<int> dq;
-    dq.push_back(7);
-    dq.push_back(8);
-    dq.push_front(6);
-    dq.push_front(5);
-    dq.push_back(9);
-    dq.pop_front();
-    dq.pop_back();
-    dq.debugging();
-    std::cout << std::endl;
-    dq.pop_back();
-    dq.debugging();
-    std::cout << std::endl;
-    dq.pop_back();
-    dq.debugging();
-    std::cout << std::endl;
-    dq.pop_back();
-    dq.pop_front();
-    dq.debugging();
+    Dequeue<int> dq[1001];
 
-    
+    for (int i = 0; i < q; i++) {
+        int id;
+        char op;
+        std::cin >> id >> op;
+
+        if (op == 'a') {
+            int copy_id;
+            std::cin >> copy_id;
+            dq[id] = dq[copy_id];
+        } else if (op == '+') {
+            char action;
+            std::cin >> action;
+            if (action == 'f') {
+                int value;
+                std::cin >> value;
+                dq[id].push_front(value);
+            } else if (action == 'b') {
+                int value;
+                std::cin >> value;
+                dq[id].push_back(value);
+            }
+        } else if (op == '-') {
+            char action;
+            std::cin >> action;
+            if (action == 'f') {
+                dq[id].pop_front();
+            } else if (action == 'b') {
+                dq[id].pop_back();
+            }
+        } else if (op == 'f') {
+            std::cout << dq[id].front() << std::endl;
+        } else if (op == 'b') {
+            std::cout << dq[id].back() << std::endl;
+        } else if (op == 's') {
+            std::cout << dq[id].size() << std::endl;
+        }
+    }
+
     return 0;
 }
 
-/* RUN
-g++ -std=c++98 dequeTest.cpp -o deque
-./deque
-*/
