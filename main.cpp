58698cc3e5cf7723819376ca5e2e78a7dd97ca5b
Auddi07 <audunnj23@ru.is>
2026-02-13 13:21:41 +0000

    remove kattis test for dll

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
     Node<int>* n2 = dll.insert(n1, 5);
 
     std::cout<<dll.erase(n2)->data<<std::endl;
-    std::cout<<dll.erase(n1)->data<<std::endl;
+    std::cout<<dll.erase(n1)->
+    data<<std::endl;
 
 }   
