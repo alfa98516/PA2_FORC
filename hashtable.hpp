a940d6b6f6b84c40b0b99fba7114fb2a720b7657
Karen Yin Guðnadóttir <kareng24@ru.is>
2026-02-13 14:11:23 +0000

    comment

--- hashtable.hpp
+++ hashtable.hpp
@@ -11,14 +11,16 @@ class HashTable {
     public:
 
         void insert(int key, T value) {
+            // insert a key-value pair to the hash table if the key does not exist, otherwise do nothing
 
         }
 
         void erase() {
-
+            // erase a given key from the hash table which is guaranteed to exist
         }
 
         T get(int key, T value) {
+            // element access, get, output the value associated with the given key if it exists, see output section
             // if hash table empty return  type (T())
             return T();
 
@@ -26,10 +28,11 @@ class HashTable {
         }
 
         void set(int key, T value) {
-            
+            // element access, set, set the value associated with a given key which is guaranteed to exist
         }
 
         int size () {
+            // output size of hash table, see output section
             return count;
         }
 }
