67402321ceba31757ab69fdab13a474749caea54
alfa98516 <alpha050916@gmail.com>
2026-02-13 12:16:15 +0000

    DELETE :: removed main.cpp from repo and put it in gitignore, now everyone can have their own main.cpp

--- dynamic_array.hpp
+++ dynamic_array.hpp
@@ -4,85 +4,104 @@
 #include <cstdlib>
 #include <ostream>
 
-template <typename T> class Dynamic_Vector {
-  T *vect;
-  size_t size; // I really have not a clue how were supposed to track the
-               // length and size of the array
-  size_t capacity;
+template <typename T>
+class Dynamic_Vector {
+    T* vect;
+    size_t size; // I really have not a clue how were supposed to track the
+                 // length and size of the array
+    size_t capacity;
 
-  void grow() {
-    capacity *= 2;
-    T *_vect = new T[capacity];
-    for (int i = 0; i < size; ++i) {
-      _vect[i] = vect[i];
-    }
-    delete[] vect;
-    vect = _vect;
-  }
-
-  void shrink() {
-    capacity /= 2;
-    T *_vect = new T[capacity];
-    for (int i = 0; i < size; ++i) {
-      _vect[i] = vect[i];
-    }
-  }
-
-public:
-  /**
-   * @brief This is the base constructor, sets the capacity at 0 and the
-   * size to 32 (this skips a couple resizes)
-   *
-   */
-  Dynamic_Vector<T>()
-      : size(0), capacity(32) { // initializing at 32 to skip a couple
-                                // resizes, this will not effect length
-    vect = new T[capacity];
-  }
-
-  /**
-   * @brief This constructor is kind of dumb and stupid, please use the
-   * default constructor and then just resize, stupid
-   * @param _capacity the capacity the user wants to initialize at
-   */
-  Dynamic_Vector<T>(size_t _capacity) : size(_capacity), capacity(32) {
-    if (capacity < size) {
-      int i = 1;
-      while (i < capacity)
-        i *= 2; // round up to nearest power of 2, improves efficiency of
-                // some functions. amortized; this constructor still runs in
-                // O(1)
-      capacity = i;
+    void grow() {
+        capacity *= 2;
+        T* _vect = new T[capacity];
+        for (int i = 0; i < size; ++i) {
+            _vect[i] = vect[i];
+        }
+        delete[] vect;
+        vect = _vect;
     }
 
-    vect = new T[capacity];
-  }
-
-  /**
-   * @brief Copy constructor, pretty standard.
-   * @param other: This is the Dynamic_Vector were copying from.
-   */
-  Dynamic_Vector(const Dynamic_Vector<T> &other) {
-    vect = new T[capacity];
-
-    for (size_t i = 0; i < size; ++i) {
-      vect[i] = other[i];
+    void shrink() {
+        capacity /= 2;
+        T* _vect = new T[capacity];
+        for (int i = 0; i < size; ++i) {
+            _vect[i] = vect[i];
+        }
+        delete[] vect;
+        vect = _vect;
     }
-  }
 
-  void erase(size_t idx) {}
+  public:
+    /**
+     * @brief This is the base constructor, sets the capacity at 0 and the
+     * size to 32 (this skips a couple resizes)
+     *
+     */
+    Dynamic_Vector<T>()
+        : size(0), capacity(32) { // initializing at 32 to skip a couple
+                                  // resizes, this will not effect length
+        vect = new T[capacity];
+    }
 
-  /**
-   * @brief Appends a new item at the end of the vector, where "end" is
-   * defined as indexing with the length variable
-   * @param item: the item were appending to the vector
-   */
-  void append(T item) {
-    if (size >= capacity)
-      grow();
-    vect[size] = item;
-    size++;
-  }
+    /**
+     * @brief This constructor is kind of dumb and stupid, please use the
+     * default constructor and then just resize, stupid
+     * @param _capacity the capacity the user wants to initialize at
+     */
+    Dynamic_Vector<T>(size_t _capacity) : size(_capacity), capacity(32) {
+        if (capacity < size) {
+            int i = 1;
+            while (i < capacity)
+                i *= 2; // round up to nearest power of 2, improves efficiency of
+                        // some functions. amortized; this constructor still runs in
+                        // O(1)
+            capacity = i;
+        }
+
+        vect = new T[capacity];
+    }
+
+    /**
+     * @brief Copy constructor, pretty standard.
+     * @param other: This is the Dynamic_Vector were copying from.
+     */
+    Dynamic_Vector(const Dynamic_Vector<T>& other) {
+        vect = new T[capacity];
+
+        for (size_t i = 0; i < size; ++i) {
+            vect[i] = other[i];
+        }
+    }
+
+    /**
+     * @brief
+     */
+    void erase(size_t idx) {
+    }
+
+    /**
+     * @brief Appends a new item at the end of the vector, where "end" is
+     * defined as indexing with the length variable
+     * @param item: the item were appending to the vector
+     */
+    void append(T item) {
+        if (size >= capacity)
+            grow();
+        vect[size] = item;
+        size++;
+    }
+
+    /**
+     * @brief I dont do any checking for bounds, its the callers responsability
+     * @param idx: the index of the item
+     */
+    T& operator[](size_t idx) const { return vect[idx]; }
+
+    /**
+     * @brief Exactly the same as the other operator[] function, this one is just for reading
+     * @param idx: the index of the item
+     */
+    const T& operator[](size_t idx) { return vect[idx]; }
 };
 
 #endif
@@ -98,11 +117,11 @@ public:
  * << 1 << 4 << 6;" works.
  */
 template <typename T>
-std::ostream &operator<<(std::ostream &out, const Dynamic_Vector<T> &Vect) {
-  for (int i = 0; i < Vect.len(); ++i) {
-    out << Vect[i] << " ";
-  }
-  return out;
+std::ostream& operator<<(std::ostream& out, const Dynamic_Vector<T>& Vect) {
+    for (int i = 0; i < Vect.len(); ++i) {
+        out << Vect[i] << " ";
+    }
+    return out;
 }
 
 #endif
